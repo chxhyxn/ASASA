9c4bcc15980b9ea5fbe8f65f8db686f16af1ebcf
chxhyxn <chxhyxn@gmail.com>
2025-04-03 15:51:01 +0900

    Initial Commit

---
+++ ASASA/ASASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASASAApp.swift
+//  ASASA
+//
+//  Created by Sean Cho on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ASASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASASA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASASA
+//
+//  Created by Sean Cho on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
